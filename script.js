fb771f9fc1e38db44b9faa3c95c04c44ca83a305
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2024-04-07 11:57:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function onesDigit(n) {
-  // your code here
+	// Check if n is a positive integer
+	if (typeof n !== 'number' || !Number.isInteger(n) || n <= 0) {
+		throw new Error("Input must be a positive integer.");
+	}
+
+	return n % 10;
 }
 
 // Do not change the code below
 
 const n = prompt("Enter Number:");
-alert(onesDigit(n));
+alert(onesDigit(Number(n)));
